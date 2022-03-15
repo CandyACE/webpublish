e3fe889643ea4851725b6004bf4bb17cdb34574e
tangweitian <tstwt@qq.com>
2022-03-15 08:03:27 -0700

    fix tray is not show in linux

--- src/main/ui/TrayManager.js
+++ src/main/ui/TrayManager.js
@@ -29,7 +29,7 @@ export default class TrayManager extends EventEmitter {
   }
 
   setIcons() {
-    this.normalIcon = nativeImage.createFromPath(path.join(__static, 'image/icon.ico'))
+    this.normalIcon = nativeImage.createFromPath(path.join(__static, 'image/twtoolsLogo.png'))
   }
 
   init() {
